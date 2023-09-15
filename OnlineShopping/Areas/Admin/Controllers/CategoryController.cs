2fb1892306437b6750906ed678ee6773e49d30d4
Samir Huseynli <samirhueynli2803@gmail.com>
2023-09-16 00:40:47 +0400

    Added Category CRUD

---
+++ OnlineShopping/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,112 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using OnlineShopping.Areas.Admin.Models;
+
+namespace OnlineShopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        public IActionResult Index()
+        {
+            var value = categoryManager.TGetList();
+            return View(value);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(AddCategoryViewModel addCategory)
+        {
+            if(ModelState.IsValid)
+            {
+                Category category = new Category()
+                {
+                    CategoryID = addCategory.CategoryID,
+                    Name = addCategory.Name,
+                    Image = UploadFile(addCategory.Image),
+                    IsActive = true
+                };
+                categoryManager.TAdd(category);
+                return RedirectToAction("Index");
+            }
+            return View(addCategory);
+        }
+
+        [HttpGet]
+        public IActionResult EditCategory(int id)
+        {
+            var value = categoryManager.TGetByID(id);
+            var editCategoryViewModel = new EditCategoryViewModel
+            {
+                ImagePreview = value.Image,
+                CategoryID = value.CategoryID,
+                Name = value.Name
+            };
+            return View(editCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult EditCategory(EditCategoryViewModel editCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                var value = categoryManager.TGetByID(editCategory.CategoryID);
+                value.Name = editCategory.Name;
+                value.Image = editCategory.Image != null ? UploadFile(editCategory.Image) : value.Image;
+                value.IsActive = value.IsActive;
+                categoryManager.TUpdate(value);
+                return RedirectToAction("Index");
+            }
+            return View(editCategory);
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = categoryManager.TGetByID(id);
+            categoryManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ChangeStatusPassive(int id)
+        {
+            var value = categoryManager.TGetByID(id);
+            value.IsActive = false;
+            categoryManager.TUpdate(value);
+            return RedirectToAction("Index");
+        }
+        
+        public IActionResult ChangeStatusActive(int id)
+        {
+            var value = categoryManager.TGetByID(id);
+            value.IsActive = true;
+            categoryManager.TUpdate(value);
+            return RedirectToAction("Index");
+        }
+
+        private string UploadFile(IFormFile? file)
+        {
+            if (file == null || file.Length == 0)
+                return null;
+
+            var path = Path.Combine(
+                        Directory.GetCurrentDirectory(), "wwwroot/ImagesFiles/CategoryImagesFiles/",
+                        file.FileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            return "/ImagesFiles/CategoryImagesFiles/" + file.FileName;
+        }
+    }
+}
---
+++ OnlineShopping/Areas/Admin/Models/EditCategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopping.Areas.Admin.Models
+{
+    public class EditCategoryViewModel
+    {
+        public int CategoryID { get; set; }
+
+        [Required(ErrorMessage = "Kateqoriya adını boş buraxa bilməzsiniz!")]
+        [MaxLength(50, ErrorMessage = "Kateqoriya adı 50 simvoldan çox ola bilməz!")]
+        [MinLength(3, ErrorMessage = "Kateqoriya adı 50 simvoldan az ola bilməz!")]
+        public string? Name { get; set; }
+        public IFormFile? Image { get; set; }
+        public string? ImagePreview { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
