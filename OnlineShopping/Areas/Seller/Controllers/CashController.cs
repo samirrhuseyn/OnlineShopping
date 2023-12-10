45a572efaefa5064b064e243c034c490da1d4329
Samir Huseynli <samirhueynli2803@gmail.com>
2023-12-11 01:05:07 +0400

    Added Cash

--- OnlineShopping/Areas/Seller/Controllers/CashController.cs
+++ OnlineShopping/Areas/Seller/Controllers/CashController.cs
@@ -15,12 +15,23 @@ namespace OnlineShopping.Areas.Seller.Controllers
             _userManager = userManager;
         }
 
-        public async Task<IActionResult>  MyCash()
+        public async Task<IActionResult> MyCash()
         {
             var value = await _userManager.FindByNameAsync(User.Identity.Name);
-            var cash = context.Orders.Where(x => x.Product.StoreID == value.StoreID).Select(x => x.Price).Sum();
+            var cash = context.Orders.Where(x => x.Product.StoreID == value.StoreID).Where(x => x.OrderDateTime.Value.Month == DateTime.Now.Month).Where(x => x.OrderDateTime.Value.Year == DateTime.Now.Year).Where(x=>x.OrderStatusID == 1).Select(x => x.Price).Sum();
+            var order = context.Orders.Where(x => x.Product.StoreID == value.StoreID).Count();
             ViewBag.Cash = cash;
+            ViewBag.Order = order;
+
+            var lastcash = context.Orders.Where(x => x.Product.StoreID == value.StoreID).Where(x => x.OrderDateTime.Value.Month == DateTime.Now.AddMonths(-1).Month).Where(x => x.OrderDateTime.Value.Year == DateTime.Now.Year).Where(x => x.OrderStatusID == 1).Select(x => x.Price).Sum();
+
+            ViewBag.LastCash = lastcash;
+
+            float? difference = cash - lastcash;
+            float? percentage = (difference * 100) / lastcash;
+            ViewBag.Percentage = percentage;
             return View();
+
         }
     }
 }
