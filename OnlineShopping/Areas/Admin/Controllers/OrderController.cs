4ac7beee918e906610db3800a503e8502f7398ff
Samir Huseynli <samirhueynli2803@gmail.com>
2023-12-21 00:41:28 +0400

    Added Orders in Admin Panel

---
+++ OnlineShopping/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopping.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        OrderManager orderManager = new OrderManager(new EfOrderDal());
+        private readonly UserManager<AppUser> _userManager;
+
+        public OrderController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            var values = orderManager.GetListByUser();
+            return View(values);
+        }
+    }
+}
